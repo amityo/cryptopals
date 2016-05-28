f91beb90bd1e419b6d70749406ae5bf5116d9561
Amit Yogev <amit.yogev90@gmail.com>
2016-05-28 20:27:29 +0300

    split to directories

---
+++ challenges/challenge1.py
@@ -0,0 +1,6 @@
+from util.converter import Converter
+
+if __name__ == "__main__":
+    hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    expected_base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    print Converter.convert_hex_to_base64(hex) == expected_base64
---
+++ util/converter.py
@@ -0,0 +1,55 @@
+import math
+import string
+
+decimal_chars = [str(i) for i in range(10)]
+hex_chars =  decimal_chars + ['a','b', 'c','d','e', 'f']
+base64_chars = list(string.ascii_uppercase) + list(string.ascii_lowercase) + [str(i) for i in range(10)] + ['+','/']
+
+
+def reverse(str):
+    return str[::-1]
+
+class Converter(object):
+    # 16 -> 10
+    @staticmethod
+    def convert_hex_to_decimal(hex):
+        return sum([int(hex_chars.index(c.lower()) * math.pow(16, idx)) for idx, c in enumerate(reverse(hex))])
+
+    # 10 -> 2
+    @staticmethod
+    def convert_decimal_to_binary_inner(decimal):
+        if decimal == 0:
+            return ""
+        else:
+            return Converter.convert_decimal_to_binary_inner(decimal / 2) + str(decimal % 2)
+          
+    @staticmethod        
+    def convert_decimal_to_binary(decimal):
+        result = Converter.convert_decimal_to_binary_inner(decimal)
+        while len(result) % 8 != 0:
+            result = "0" + result
+        return result
+            
+    # 2 -> 10
+    @staticmethod
+    def convert_binary_to_decimal(binary):
+        return sum([int(int(c) * math.pow(2, idx)) for idx, c in enumerate(reverse(binary))])
+
+    # 16 -> 2
+    @staticmethod
+    def convert_hex_to_binary(hex):
+        decimal = Converter.convert_hex_to_decimal(hex)
+        return Converter.convert_decimal_to_binary(decimal)
+
+    # 2 -> 64
+    @staticmethod
+    def convert_binary_to_base64(binary):
+        decimals = [Converter.convert_binary_to_decimal(binary[i:i+6]) for i in range(0, len(binary), 6)]
+        return ''.join([base64_chars[decimal] for decimal in decimals])
+
+    # 16 -> 64
+    @staticmethod
+    def convert_hex_to_base64(hex):
+        binary = Converter.convert_hex_to_binary(hex)
+        return Converter.convert_binary_to_base64(binary)
+
